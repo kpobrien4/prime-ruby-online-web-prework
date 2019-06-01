373531cba8c9a5a07c9ec4b4af19d1cd14018d98
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-01 23:43:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+  false if num < 2
+  (2...num).each do |factor|
+    if (num % factor).zero? 
+      return false
+    end
+end
+true # after checking all numbers and not receiving false
+end
